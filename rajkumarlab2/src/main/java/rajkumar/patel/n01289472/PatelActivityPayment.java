81528aeeb12392250508a79dc6d5b9a82deafb47
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-11-05 11:20:17 -0500

    This is my First Commit

---
+++ rajkumarlab2/src/main/java/rajkumar/patel/n01289472/PatelActivityPayment.java
@@ -0,0 +1,18 @@
+package rajkumar.patel.n01289472;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//Name: Rajkumar Patel
+//Student ID: N01289472
+//Section: RNA
+
+public class PatelActivityPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patel_payment);
+    }
+}
