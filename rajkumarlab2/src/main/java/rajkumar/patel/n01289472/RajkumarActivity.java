60f43c36fa0b46b772177cf1162814dc2f6061c5
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-11-07 19:15:07 -0500

    Main Screen Activity
    -Added Company Logo
    -Added style to button
    -Fixed landscape layout
    -Added code to make button switch to Second Activity

--- rajkumarlab2/src/main/java/rajkumar/patel/n01289472/RajkumarActivity.java
+++ rajkumarlab2/src/main/java/rajkumar/patel/n01289472/RajkumarActivity.java
@@ -2,7 +2,10 @@ package rajkumar.patel.n01289472;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 //Name: Rajkumar Patel
 //Student ID: N01289472
@@ -14,5 +17,17 @@ public class RajkumarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rajkumar);
+    }
+
+    public void callIntent(View view) {
+        switch (view.getId()) {
+            case R.id.rajkumarBtn:
+            case R.id.rajkumarBtn2:
+                Intent intent = new Intent(RajkumarActivity.this, PatelActivityHomepage.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
