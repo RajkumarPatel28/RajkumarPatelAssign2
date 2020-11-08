cd046bbd060001c1415b58936c616b52ef458448
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-11-07 21:12:51 -0500

    Rajkumar Activity
    -Added Alert Dialog to Main Screen when back key is pressed

--- rajkumarlab2/src/main/java/rajkumar/patel/n01289472/RajkumarActivity.java
+++ rajkumarlab2/src/main/java/rajkumar/patel/n01289472/RajkumarActivity.java
@@ -1,7 +1,9 @@
 package rajkumar.patel.n01289472;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +19,29 @@ public class RajkumarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rajkumar);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage("Would you like to exit?");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                RajkumarActivity.this.finish();
+            }
+        });
+        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog dialogMessage = dialog.create();
+        dialogMessage.setTitle("Exit App");
+        dialogMessage.setIcon(R.drawable.question_mark);
+        dialogMessage.show();
     }
 
     public void callIntent(View view) {
