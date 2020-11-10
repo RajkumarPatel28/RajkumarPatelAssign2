ff5c55ab6d4676ab0e5de17ba0ea40b5830149e9
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-11-10 02:47:18 -0500

    Payment Activity

--- rajkumarlab2/src/main/java/rajkumar/patel/n01289472/PatelActivityCheckout.java
+++ rajkumarlab2/src/main/java/rajkumar/patel/n01289472/PatelActivityCheckout.java
@@ -2,7 +2,10 @@ package rajkumar.patel.n01289472;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 //Name: Rajkumar Patel
 //Student ID: N01289472
@@ -14,5 +17,16 @@ public class PatelActivityCheckout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patel_checkout);
+        setTitle("Checkout");
+
+        ImageButton rajkumarIBtn2 = findViewById(R.id.rajkumarIBtn2);
+
+        rajkumarIBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( PatelActivityCheckout.this,PatelActivityPayment.class);
+                startActivity(intent);
+            }
+        });
     }
 }
